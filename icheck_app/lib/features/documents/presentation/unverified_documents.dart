5dfac01bed76d38a9f0111300df73a029b8e9085
Job Lipat <lipatjobj@gmail.com>
2022-07-04 14:50:41 +0800

    added pages

---
+++ icheck_app/lib/features/documents/presentation/unverified_documents.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UnverifiedDocuments extends StatelessWidget {
+  const UnverifiedDocuments({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Implement this page");
+  }
+}
