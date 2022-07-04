fc9f11189cc7a4ad632bf08259a356560eeadcfc
Job Lipat <lipatjobj@gmail.com>
2022-07-04 14:50:59 +0800

    added pages

---
+++ icheck_app/lib/features/authentication/presentation/register_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterAccount extends StatelessWidget {
+  const RegisterAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Implement this page");
+  }
+}
---
+++ icheck_app/lib/features/authentication/presentation/register_verify_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterVerifyAccount extends StatelessWidget {
+  const RegisterVerifyAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Implement this page");
+  }
+}
